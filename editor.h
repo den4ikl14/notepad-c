566351cc7613f27218d8c29df9054fa176ee0e28
den4ikl14 <denislubivij@gmail.com>
2026-05-25 23:53:26 +0300

    Update editor.h

--- editor.h
+++ editor.h
@@ -1,26 +1,32 @@
 #pragma once
 #include <string>
 #include <vector>
-#include <fstream>
 #include <windows.h>
 
 #define MAX_UNDO 100
 
 struct State {
     std::vector<std::string> lines;
-    int cursorX, cursorY;
+    int cursorX = 0;
+    int cursorY = 0;
 };
 
 struct Editor {
     std::vector<std::string> lines;
-    int cursorX, cursorY;
-    int scrollY;
+    int cursorX = 0;
+    int cursorY = 0;
+    int scrollY = 0;
     std::string filename;
-    bool modified;
+    bool modified = false;
     std::string clipboard;
     State undoStack[MAX_UNDO];
     State redoStack[MAX_UNDO];
-    int undoTop, redoTop;
+    int undoTop = -1;
+    int redoTop = -1;
+
+    bool selecting = false;
+    int selStartX = 0;
+    int selStartY = 0;
 
     void run();
     void render();
@@ -28,4 +34,14 @@ struct Editor {
     void saveState();
     void saveFile();
     void adjustScroll();
+    void findText();
+    void replaceText();
+    void copySelection();
+    void cutSelection();
+    void pasteClipboard();
+    std::string getSelectedText();
+    void deleteSelected();
+    bool hasSelection();
 };
+
+void gotoxy(int x, int y);
