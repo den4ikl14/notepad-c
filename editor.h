9d5274bdddabe84575afdd3d972ec931f2a650da
den4ikl14 <denislubivij@gmail.com>
2026-05-28 22:25:21 +0300

    Update editor.h

--- editor.h
+++ editor.h
@@ -11,6 +11,15 @@ struct State {
     int cursorY = 0;
 };
 
+struct ThemeStyle {
+    WORD header = 0;
+    WORD text = 0;
+    WORD lineNum = 0;
+    WORD selection = 0;
+    WORD status = 0;
+    WORD prompt = 0;
+};
+
 struct Editor {
     std::vector<std::string> lines;
     int cursorX = 0;
@@ -18,7 +27,9 @@ struct Editor {
     int scrollY = 0;
     std::string filename;
     bool modified = false;
-    std::string clipboard;
+    bool darkTheme = false;
+    bool running = true;
+    std::string lastSearch;
     State undoStack[MAX_UNDO];
     State redoStack[MAX_UNDO];
     int undoTop = -1;
@@ -33,15 +44,26 @@ struct Editor {
     void handleInput();
     void saveState();
     void saveFile();
+    void openFile();
     void adjustScroll();
     void findText();
+    void findNext();
     void replaceText();
     void copySelection();
     void cutSelection();
     void pasteClipboard();
+    void copyToClipboard(const std::string& text);
     std::string getSelectedText();
     void deleteSelected();
     bool hasSelection();
+    void toggleTheme();
+    void showHelp();
+    ThemeStyle currentTheme() const;
+    std::string readStatusInput(const std::string& prompt);
+    bool tryExit();
+    void getViewport(int& screenWidth, int& editLineCount) const;
+    void getWindowLayout(int& winTop, int& winBottom, int& width) const;
+    void showStatusMessage(const std::string& msg, bool waitKey = true);
 };
 
 void gotoxy(int x, int y);
